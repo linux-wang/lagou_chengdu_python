6a5dbe8780fb5ecad25b1e4480482d5b29074162
wang <wang@wangdeMacBook-Pro.local>
2018-08-03 10:48:06 +0800

    初步开发完成

--- parse_lg.py
+++ parse_lg.py
@@ -5,9 +5,8 @@ import json
 from requests_html import HTMLSession
 import time
 from selenium import webdriver
+import random
 
-
-cookie = get_cookie('https://www.lagou.com/jobs/list_Python?px=default&city=%E6%88%90%E9%83%BD#filterBox')
 
 # get html source code
 def get_html(url):
@@ -16,40 +15,15 @@ def get_html(url):
     # return r.text
     driver = webdriver.chrome.webdriver.WebDriver()
     driver.get(url)    
-    driver.delete_all_cookies()
 
     driver.implicitly_wait(10)
+    time.sleep(random.randint(4, 10))
 
-    # driver.find_element_by_class_name('pager_next').get_attribute('class')
-    # pager_next pager_next_disabled
-    time.sleep(10)
     return driver.page_source.encode('utf-8')
 
 
-# get job_list_pages_link
-def get_page_link(start_url):
-    page_link = set()
-    page_link.add(start_url)
-
-    c = get_html(start_url)
-    soup_c = BeautifulSoup(c, 'lxml')
-    is_last = soup_c.find('a', class_='page_no pager_next_disabled')
-
-    while is_last:
-        next_url = soup_c.find_all('a', class_='page_no')[-1].get('href')
-        page_link.add(next_url)
-
-        c = get_html(next_url)
-        soup_c = BeautifulSoup(c, 'lxml')
-        is_last = soup_c.find('a', class_='page_no pager_next_disabled')
-        time.sleep(5)
-
-    return page_link
-
-
 # get job_links
-def get_job_list(url):
-    content = get_html(url)
+def get_job_list(content):
     soup_c = BeautifulSoup(content, 'lxml')
 
     job_links = soup_c.find_all('a', class_='position_link')
@@ -58,14 +32,11 @@ def get_job_list(url):
     return job_links
 
 
-def get_job_info(url):
-    content = get_html(url)
-    # print(content)
+def get_job_info(content):
     soup = BeautifulSoup(content, 'lxml')
 
     title = soup.title.text
-    # company_name = soup.find('div', class_='company').text
-    company_name = soup.find('h2', class_='fl').text
+    company_name = soup.find('div', class_='company').text
     position = soup.find('span', class_='name').text
     salary = soup.find('span', class_='salary').text
 
@@ -110,28 +81,47 @@ def get_job_info(url):
     return job_info, company_info
 
 
-def main():
-    start_url = 'https://www.lagou.com/zhaopin/Python/'
-    page_links = get_page_link(start_url)
-    # print(page_links)
-
+def work(start_url):
     job_links = set()
 
-    jbl = get_job_list(start_url)
-    for pl in page_links:
-        job_links.update(pl)
-    # job_links.update(jbl)
-    # print(job_links)
+    driver = webdriver.chrome.webdriver.WebDriver()
+    driver.get(start_url)
+    driver.implicitly_wait(10)
+    content = driver.page_source.encode('utf-8')
+    time.sleep(random.randint(4, 10))
 
-    for jl in job_links:
-        print(jl)
-        job_info, com_info = get_job_info(jl)
-        with open('result.txt', 'a') as f:
-            f.write(json.dumps(job_info) + '    ' + json.dumps(com_info) + '\n')
+    job_link = get_job_list(content)
+    job_links.update(job_link)
+
+    is_last = driver.find_element_by_class_name('pager_next').get_attribute('class')
+    while is_last != 'pager_next pager_next_disabled':
+        driver.find_element_by_class_name('pager_next').click()
+        driver.implicitly_wait(10)
+        content = driver.page_source.encode('utf-8')
+
+        job_link = get_job_list(content)
+        job_links.update(job_link)
+
+        is_last = driver.find_element_by_class_name('pager_next').get_attribute('class')
+        print(is_last)
+        time.sleep(random.randint(4, 10))
+
+    # for jl in job_links:
+    #     print(jl)
+    #     driver.get(jl)
+    #     job_content = driver.page_source.encode('utf-8')
+    #     driver.implicitly_wait(10)
+
+    #     job_info, com_info = get_job_info(job_content)
+    #     with open('result.txt', 'a') as f:
+    #         f.write(json.dumps(job_info) + '    ' + json.dumps(com_info) + '\n')
+
+    #     time.sleep(random.randint(4, 10))
+
+
+def main():
+    start_url = 'https://www.lagou.com/jobs/list_Python?px=default&city=%E6%88%90%E9%83%BD#filterBox'
+    work(start_url)
 
 if __name__ == '__main__':
     main()
-
-
-# start_url = 'https://www.lagou.com/zhaopin/Python/?filterOption=3'
-# next_url = 'https://www.lagou.com/zhaopin/Python/2/'
